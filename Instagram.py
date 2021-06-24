57dbf5508bd16d05693955317e645798a24bd6a3
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-24 11:45:17 +0530

    Instagram bot

---
+++ Instagram.py
@@ -0,0 +1,27 @@
+from instapy import InstaPy
+from instapy import smart_run
+
+my_username='akshi.sinharajput'
+my_password='Akshi@123'
+
+
+
+session= InstaPy(username=my_username,
+	             password=my_password,
+	             headless_browser = False)
+
+
+with smart_run(session):
+	session.set_relationship_bounds(enabled =True,
+		                           delimit_by_numbers =True,
+		                           max_followers = 500,
+		                           min_followers = 30,
+		                           min_following = 50)
+	session.set_do_follow(True,percentage=100)
+	session.set_dont_like(['nsfw','kia','ford'])
+	session.like_by_tags(['bmw','mercedes'], amount=10)
+
+
+	
+	
+	
